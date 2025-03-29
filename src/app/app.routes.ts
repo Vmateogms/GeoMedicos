e8d01f7a1d050b096c0fb55d752ec666a08ffc45
Vmateogms <vicentemateogarciamerino@gmail.com>
2025-03-29 01:06:29 +0100

    Agregué nuevas funcionalidades al módulo de autenticación

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,34 @@
+import { RegisterComponent } from './public/register/register.component';
+import { LoginComponent } from './public/login/login.component';
+import { EnfermedadesListComponent } from './public/enfermedades-list/enfermedades-list.component';
+import { AreaPersonalUsuariosComponent } from './pages/User/pages/area-personal-usuarios/area-personal-usuarios.component';
+import { AreaPersonalMedicosComponent } from './pages/Medicos/pages/area-personal-medicos/area-personal-medicos.component';
 import { Routes } from '@angular/router';
-import { HomeComponent } from './User/pages/home/home.component';
+import { HomeComponent } from './public/home/home.component';
+import { Page404Component } from './pages/page404/page404.component';
+
+
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent},
-    {path: 'home', component: HomeComponent}
+
+
+    //rutas publicas
+    {path: 'home', component: HomeComponent},
+    {path: 'lista-enfermedades', component: EnfermedadesListComponent},
+    {path: 'login', component:LoginComponent},
+    {path: 'register', component: RegisterComponent},
+
+    // rutas de usuario
+    {path: 'usario/areapersonal', component: AreaPersonalUsuariosComponent},
+    //rutas de medico
+    {path: 'medico/areapersonal', component: AreaPersonalMedicosComponent},
+    
+
+    //rutas de administrador
+
+
+
+    //rutas por defecto 
+    {path: '', pathMatch: 'full', redirectTo: 'home'},
+    {path: '**', component: Page404Component}
 ];
