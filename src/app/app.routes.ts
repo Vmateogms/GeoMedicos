1e131b0c8d673363b11d81889246540b4a5a380a
Vmateogms <vicentemateogarciamerino@gmail.com>
2025-03-29 01:10:08 +0100

    test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { AreaPersonalAdminComponent } from './pages/Admin/pages/area-personal-admin/area-personal-admin.component';
 import { RegisterComponent } from './public/register/register.component';
 import { LoginComponent } from './public/login/login.component';
 import { EnfermedadesListComponent } from './public/enfermedades-list/enfermedades-list.component';
@@ -6,6 +7,8 @@ import { AreaPersonalMedicosComponent } from './pages/Medicos/pages/area-persona
 import { Routes } from '@angular/router';
 import { HomeComponent } from './public/home/home.component';
 import { Page404Component } from './pages/page404/page404.component';
+import { authGuard } from './seguridad/guards/auth.guard';
+import { roleGuard } from './seguridad/role-guard';
 
 
 
@@ -13,19 +16,17 @@ export const routes: Routes = [
 
 
     //rutas publicas
-    {path: 'home', component: HomeComponent},
-    {path: 'lista-enfermedades', component: EnfermedadesListComponent},
-    {path: 'login', component:LoginComponent},
-    {path: 'register', component: RegisterComponent},
+    {path: 'home', component: HomeComponent, canActivate: [authGuard]},
+    {path: 'lista-enfermedades', component: EnfermedadesListComponent, canActivate: [authGuard]},
+    {path: 'login', component:LoginComponent, canActivate: [authGuard]},
+    {path: 'register', component: RegisterComponent, canActivate: [authGuard]},
 
     // rutas de usuario
-    {path: 'usario/areapersonal', component: AreaPersonalUsuariosComponent},
+    {path: 'usario/areapersonal', component: AreaPersonalUsuariosComponent, canActivate: [roleGuard], data: {expectedRole: 'PACIENTE'} },
     //rutas de medico
-    {path: 'medico/areapersonal', component: AreaPersonalMedicosComponent},
-    
-
+    {path: 'medico/areapersonal', component: AreaPersonalMedicosComponent, canActivate: [roleGuard], data: {expectedrole: 'DOCTOR'}},
     //rutas de administrador
-
+    {path: 'admin/areapersonal', component: AreaPersonalAdminComponent, canActivate: [roleGuard], data: {expectedRole: 'ADMON'}},
 
 
     //rutas por defecto 
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -19,45 +19,52 @@ export class AuthService {
     if(storedUser) {
       this.currentUserSubject.next(JSON.parse(storedUser));
     }
-   }
+  }
 
+    isAuthenticated(): boolean {
+    return this.currentUserSubject.value !== null;
+    }
+
+    getRole(): String | null {
+    return this.currentUserSubject.value?.role || null;
+    }
 
    login(credentials: {correo: String; password: string}): Observable<IUser> { //cuando un user inicia sesion este metodo envia credencias al back si es correcto guarda al user en LocalStorage (para persistencia) y en BehaviorSubject(para state managment), emite al user a todos los componentes suscritos
     return this.http.post<IUser>('/api/login', credentials).pipe(  //lo de '/api/login' consultar con sergio (backend) !!!!!
       tap(user => {
         //Guardar usuario en localStorage del navegador
-        localStorage.setItem('CurrentUser', JSON.stringify(user));
+        localStorage.setItem('currentUser', JSON.stringify(user));
         this.currentUserSubject.next(user);
       })
     )
-   }
+    }
 
    logout() { //limpia completamente la sesion 
     localStorage.removeItem('currentUser'); //borra los datos del localStorage
     this.currentUserSubject.next(null) //resetea el estado del user a null
-   }
+    }
 
    //METODOS PARA VERIFICAR LOS ROLES
 
    isPaciente(): boolean { //true or false
     return this.currentUserSubject.value?.role === 'PACIENTE'
-   }
+    }
 
-   isDoctor(): boolean {
+    isDoctor(): boolean {
     return this.currentUserSubject.value?.role === 'DOCTOR'
-   }
+    }
 
-   isAdmin(): boolean {
+    isAdmin(): boolean {
     return this.currentUserSubject.value?.role === 'ADMON'
-   }
+    }
 
 
    //Guard para proteger rutas
 
-   canActivate(allowedRules: String[]): boolean {
+  canActivate(allowedRules: String[]): boolean {
     const currentUer = this.currentUserSubject.value;
     return currentUer ? allowedRules.includes(currentUer.role) : false;
-   }
+    }
 
 
 }
